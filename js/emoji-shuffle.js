d4364480fe0f4ddc5ca6ff8909f3ae2fa87ce157
Claire Nord <cnord@mit.edu>
2016-08-10 21:14:28 +0900

    Add arbitrary random emoji

--- js/emoji-shuffle.js
+++ js/emoji-shuffle.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
+  // code based off of https://code.hackmit.org's emoji footer :D
   // click the bird
+  var EMOJIS = [
+    'chicken',
+    'penguin',
+    'baby_chick',
+    'bird',
+    'egg'
+  ];
 
+  // select a new emoji
+  var newemoji = function() {
+    return 'em-' + EMOJIS[Math.floor(Math.random() * EMOJIS.length)];
+  }
+  
   $('#emoji').click( function() {
-    $(this).toggleClass('em-baby_chick').toggleClass('em-bird');
+      $(this).attr("class", 'em ' + newemoji());
     });
 
 }());
